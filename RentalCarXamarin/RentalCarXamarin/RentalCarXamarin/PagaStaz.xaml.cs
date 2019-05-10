32f47ce1399150a96e8275121fa05edcf2f18004
DavideVita <davide.vitaletti@gmail.com>
2019-05-10 20:58:13 +0200

    PagaStaz_PagaOra_ConfermaEmail

---
+++ RentalCarXamarin/RentalCarXamarin/RentalCarXamarin/PagaStaz.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RentalCarXamarin
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PagaStaz : ContentPage
+	{
+		public PagaStaz ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
