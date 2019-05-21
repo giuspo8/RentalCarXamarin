56c3093d868e5781fa82f3c7d8a405f92912ed3a
giusp8 <giuspo89@hotmail.com>
2019-05-21 19:05:29 +0200

    Implementata richiesta server Car

--- RentalCarXamarin/RentalCarXamarin/RentalCarXamarin/CarItem.cs
+++ RentalCarXamarin/RentalCarXamarin/RentalCarXamarin/CarItem.cs
@@ -6,21 +6,24 @@ namespace RentalCarXamarin
 {
     public class CarItem
     {
-        //int resIdImage { get; set; }//id dell'immagine della macchina
+        public String Image { get; set; }//id dell'immagine della macchina
         public String Model { get; set; }//nome dell'auto
         public String ClassCar { get; set; }//classe dell'auto
         public double Pricegg { get; set; }//prezzo giornaliero
         public String Shift { get; set; }//cambio
         public int Number { get; set; }//numero di posti
 
-        public CarItem( String Model, String ClassCar, double Pricegg, String Shift, int Number)
+        
+
+        public CarItem(String Model,String Shift, String ClassCar, int Number,double Pricegg,String Image)
         {
-            //this.resIdImage = resIdImage;
+            this.Image = Image;
             this.Model = Model;
             this.ClassCar = ClassCar;
             this.Pricegg = Pricegg;
             this.Shift = Shift;
             this.Number = Number;
         }
+        public CarItem() { }
     }
 }
--- RentalCarXamarin/RentalCarXamarin/RentalCarXamarin/SceltaAuto.xaml.cs
+++ RentalCarXamarin/RentalCarXamarin/RentalCarXamarin/SceltaAuto.xaml.cs
@@ -16,84 +16,25 @@ namespace RentalCarXamarin
         public SceltaAuto()
         {
             InitializeComponent();
-            BindingContext = new CarChoosingModel();
-           
-
-        }
-
-        //parte da spostare in un'altro file eventualmente
-        public class Car : BindableObject
-        {
-            //definiamo la classe car che estende la classe BindableObject 
-            //e quindi ci abilita a sincronizzare i dati
-            //di seguito le properties della classe Car
-            public String image { get; set; }
-            public String model { get; set; }
-            public String type { get; set; }
-            public String shift { get; set; }
-            public int numberOfPassengers { get; set; }
-            public double price { get; set; }
+            getCar(new ServerRequest("http://rentalcar.altervista.org/leggi_auto.php"));
         }
 
         //metodo per gestire il click su un elemento della lista
         public async void CarTapped (object sender,ItemTappedEventArgs e)
         {
             //creiamo un oggetto di Car a partire dall'elemento cliccato
-            Car item = (Car)e.Item;
+            CarItem item = (CarItem)e.Item;
             //salviamo tutti i suoi attributi sul dizionario Properties
-            Application.Current.Properties["image"] = item.image;
-            Application.Current.Properties["model"] = item.model;
-            Application.Current.Properties["type"] = item.type;
-            Application.Current.Properties["shift"] = item.shift;
-            Application.Current.Properties["numberOfPassengers"] = item.numberOfPassengers;
-            Application.Current.Properties["price"] = item.price;
+            Application.Current.Properties["image"] = item.Image;
+            Application.Current.Properties["model"] = item.Model;
+            Application.Current.Properties["type"] = item.ClassCar;
+            Application.Current.Properties["shift"] = item.Shift;
+            Application.Current.Properties["numberOfPassengers"] = item.Number;
+            Application.Current.Properties["price"] = item.Pricegg;
             //passiamo alla prossima Page
             await this.Navigation.PushAsync(new RiepilogoPrenotazione());
         }
-
-        public class CarChoosingModel : BindableObject
-        {
-            //altra classe che estende bindableobject
-            //abbiamo una lista di Car
-            List<Car> carItems;
-            //nota bene CarItems con c maiuscolo. è la proprietà della variabile carItems
-            //lo utilizziamo per fare get e set di carItems
-            public List<Car> CarItems
-            {
-                get
-                {
-                    return carItems;
-                }
-                set
-                {
-                    carItems = value;//a carItems viene assegnato il valore passatogli con il set
-                    //OnPropertyChanged semplicemente notifica che è avvenuta una modifica di CarItems
-                    OnPropertyChanged("CarItems");
-                }
-            }
-
-            public List<Car> CarList { get; } = new List<Car>();
-
-            //è il costruttore di CarChoosingModel
-            public CarChoosingModel()
-            {
-
-                //associamo alla lista i valori che dovremo ottenere dal server
-                getCar(new ServerRequest("http://rentalcar.altervista.org/leggi_auto.php"));
-
-                CarItems = new List<Car>{
-
-
-                new Car {image = "cinquecento.png", model = "Fiat Cinquecento", type="Economy", shift="manuale",numberOfPassengers=5,price=100.00},
-
-                new Car {image = "clio.png", model = "Renault Clio", type="Compatta", shift="manuale",numberOfPassengers=5,price=100.00},
-
-                new Car {image = "golf.png", model = "Volswagen Golf", type="Luxury", shift="automatico",numberOfPassengers=5,price=100.00}
-
-            };
-                
-
-            }
+       
 
             public async void getCar(ServerRequest sr)
             {
@@ -102,21 +43,21 @@ namespace RentalCarXamarin
                 {
                     var result = response.Content.ReadAsStringAsync().Result;
                     Dictionary<string, CarItem> result_car = JsonConvert.DeserializeObject<Dictionary<string, CarItem>>(result);
-                   
+                   List<CarItem> cars = new List<CarItem>();
 
-                    foreach (KeyValuePair<string, CarItem> entry in result_car)
+                foreach (KeyValuePair<string, CarItem> entry in result_car)
                     {
-                        
-                        CarItems.Add(new Car { image = "golf.png", model=entry.Value.Model, type = entry.Value.ClassCar, shift = entry.Value.Shift,
-                            numberOfPassengers = 10, price = 100.00
-                        });
+                            cars.Add(new CarItem (entry.Value.Model,entry.Value.Shift,entry.Value.ClassCar,
+                            entry.Value.Number,entry.Value.Pricegg,entry.Value.Image));
                     }
+                listView.ItemsSource = cars;
                 }
-                else { }
-                    //await DisplayAlert("Attenzione", "Nothing retrieved from the server", "OK");
+                else { await DisplayAlert("Attenzione", "Nothing retrieved from the server", "OK"); }
+                    
             }
-        }
-
+            
+    }
+    
 
     }
-}
+
