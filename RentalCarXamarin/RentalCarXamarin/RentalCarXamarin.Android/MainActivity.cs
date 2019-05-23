8a7a3c7a41d73ee850e1dd1516c2a9e13b0e44fc
DavideVita <davide.vitaletti@gmail.com>
2019-05-23 19:17:37 +0200

    FAQ

--- RentalCarXamarin/RentalCarXamarin/RentalCarXamarin.Android/MainActivity.cs
+++ RentalCarXamarin/RentalCarXamarin/RentalCarXamarin.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace RentalCarXamarin.Droid
 {
-    [Activity(Label = "RentalCarXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "RCMx", Icon = "@drawable/car_prova", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- RentalCarXamarin/RentalCarXamarin/RentalCarXamarin/faq.xaml.cs
+++ RentalCarXamarin/RentalCarXamarin/RentalCarXamarin/faq.xaml.cs
@@ -10,89 +10,69 @@ using Xamarin.Forms.Xaml;
 
 namespace RentalCarXamarin
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class faq : ContentPage
-	{
-        public faq ()
-		{
-			InitializeComponent ();
-            BindingContext = new ListViewStringsViewModel();
-
-        }
-        public async void backToHomeButton(object sender, EventArgs e)
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class faq : ContentPage
+    {
+        public faq()
         {
-            //toglie tutte le pagine dallo stack e va alla pagina iniziale
-            await this.Navigation.PopToRootAsync();
-        }
+            InitializeComponent();
+            List<ListItem> listItems = new List<ListItem>();
+            listItems = new List<ListItem> {
+                new ListItem {item="Dopo aver prenotato, è possibile cambiare la vettura scelta ?",index=0},
+                new ListItem {item= "Cosa succede se la macchina non viene riconsegnata nei tempi previsti ?",index=1},
+                new ListItem {item = "Di che cosa ho bisogno per noleggiare un' auto ?",index=2},
+                new ListItem {item = "Cosa faccio se mi si dovessere rompere l'auto ?",index=3},
+                new ListItem {item= "Posso prenotare un' auto per conto di terzi ?",index=4},
+                new ListItem {item = "E' tutto incluso nel prezzo del noleggio ?",index=5}
 
-        async void ListViewItemTapped(object sender, ItemTappedEventArgs e)
+        };
+            listfaq.ItemsSource = listItems;
+        }
+        public class ListItem
+        {
+            public string item { get; set; }
+            public int index { get; set; }
+        }
+        public async void ListViewItemTapped(object sender, ItemTappedEventArgs e)
         {
             //facciamo il controllo dell' elemento cliccato
-            string item = (string)e.Item;
-            if (item == "Dopo aver prenotato, è possibile cambiare la macchina ?")
-            {
-                //stampa a video la risposta
-                await DisplayAlert("Risposta", "No, non è possibile.", "OK");
-            }
-            else if (item == "Cosa succede se non si riconsegna la macchina ?")
-            {
-                //stampa a video la risposta
-                await DisplayAlert("Risposta", "Il cliente verrà contattato.", "OK");
-            }
-            else if (item == "Di cosa ho bisogno per noleggiare un' auto ?")
-            {
-                //stampa a video la risposta
-                await DisplayAlert("Risposta", "Per prenotare un' auto, serve una carta di credito", "OK");
-            }
-            else if (item == "Cosa faccio se mi si rompe la macchina ?")
-            {
-                //stampa a video la risposta
-                await DisplayAlert("Risposta", "Devi chiamarci e noi ti veniamo a prendere", "OK");
-            }
-            else if (item == "Posso prenotare un' auto per conto di qualcun altro ?")
-            {
-                //stampa a video la risposta
-                await DisplayAlert("Risposta", "No non è possibile", "OK");
-            }
-            else if (item == "E' tutto incluso nel prezzo del noleggio ?")
-            {
-                //stampa a video la risposta
-                await DisplayAlert("Risposta", "sì è tutto incluso", "OK");
-            }
-            ((ListView)sender).SelectedItem = null;
-        }
+            ListItem l = (ListItem)e.Item;
+            int index = l.index;
 
-        public class ListViewStringsViewModel : BindableObject
-        {
-            List<string> items;
-            public List<string> Items
+            switch (index)
             {
-                get
-                {
-                    return items;
-                }
-                set
-                {
-                    items = value;
-                    OnPropertyChanged("Items");
-                }
-            }
-
-            public ListViewStringsViewModel()
-            {
-                //insieme delle domande
-                Items = new List<string>
-                    {
-                        "Dopo aver prenotato, è possibile cambiare la macchina ?",
-                        "Cosa succede se non si riconsegna la macchina ?",
-                        "Di cosa ho bisogno per noleggiare un' auto ?",
-                        "Cosa faccio se mi si rompe la macchina ?",
-                        "Posso prenotare un' auto per conto di qualcun altro ?",
-                        "E' tutto incluso nel prezzo del noleggio ?"
-                    };
+                case 0:
+                    //stampa a video la risposta
+                    await DisplayAlert("Risposta", "No, non è possibile.", "OK");
+                    break;
+                case 1:
+                    //stampa a video la risposta
+                    await DisplayAlert("Risposta", "Verrà contattato.", "OK");
+                    break;
+                case 2:
+                    //stampa a video la risposta
+                    await DisplayAlert("Risposta", "Per prenotare un auto, serve una carta di credito", "OK");
+                    break;
+                case 3:
+                    //stampa a video la risposta
+                    await DisplayAlert("Risposta", "Deve chiamarci e noi La veniamo a prendere", "OK");
+                    break;
+                case 4:
+                    //stampa a video la risposta
+                    await DisplayAlert("Risposta", "No, non è possibile", "OK");
+                    break;
+                case 5:
+                    //stampa a video la risposta
+                    await DisplayAlert("Risposta", "Sì, è tutto incluso", "OK");
+                    break;
             }
         }
-
-
     }
 }
+
+        
+
+
+
+        
+    
