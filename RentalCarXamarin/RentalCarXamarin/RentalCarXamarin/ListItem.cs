ee493c9761d55df4427e23dbc4f99464625e3ff4
Marco <marcogovernatori@gmail.com>
2019-06-03 19:57:33 +0200

    faq

---
+++ RentalCarXamarin/RentalCarXamarin/RentalCarXamarin/ListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+namespace RentalCarXamarin
+{
+    public class ListItem 
+    {
+        
+        public string item { get; set; }
+        public int index { get; set; }
+        public string rispo { get; set; }
+       
+    }
+}
--- RentalCarXamarin/RentalCarXamarin/RentalCarXamarin/faq.xaml.cs
+++ RentalCarXamarin/RentalCarXamarin/RentalCarXamarin/faq.xaml.cs
@@ -13,61 +13,35 @@ namespace RentalCarXamarin
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class faq : ContentPage
     {
+
         public faq()
         {
             InitializeComponent();
             List<ListItem> listItems = new List<ListItem> {
-                new ListItem {item="Dopo aver prenotato, è possibile cambiare la vettura scelta ?",index=0},
-                new ListItem {item= "Cosa succede se la macchina non viene riconsegnata nei tempi previsti ?",index=1},
-                new ListItem {item = "Di che cosa ho bisogno per noleggiare un' auto ?",index=2},
-                new ListItem {item = "Cosa faccio se mi si dovessere rompere l'auto ?",index=3},
-                new ListItem {item= "Posso prenotare un' auto per conto di terzi ?",index=4},
-                new ListItem {item = "E' tutto incluso nel prezzo del noleggio ?",index=5}
+
+                new ListItem {item="Dopo aver prenotato, è possibile cambiare la vettura scelta ?",index=0,rispo="No, non è possibile."},
+
+                new ListItem {item= "Cosa succede se la macchina non viene riconsegnata nei tempi previsti ?",index=1,rispo="Verrà contattato."},
+
+                new ListItem {item = "Di che cosa ho bisogno per noleggiare un' auto ?",index=2,rispo="Per prenotare un auto, serve una carta di credito"},
+
+                new ListItem {item = "Cosa faccio se mi si dovessere rompere l'auto ?",index=3,rispo="Deve chiamarci e noi La veniamo a prendere"},
+
+                new ListItem {item= "Posso prenotare un' auto per conto di terzi ?",index=4,rispo="No, non è possibile."},
+
+                new ListItem {item = "E' tutto incluso nel prezzo del noleggio ?",index=5,rispo="Sì, è tutto incluso"}
+
+
 
         };
+
             listfaq.ItemsSource = listItems;
         }
-        public class ListItem
-        {
-            public string item { get; set; }
-            public int index { get; set; }
-        }
-        public async void ListViewItemTapped(object sender, ItemTappedEventArgs e)
-        {
-            //facciamo il controllo dell' elemento cliccato
-            ListItem l = (ListItem)e.Item;
-            int index = l.index;
 
-            switch (index)
-            {
-                case 0:
-                    //stampa a video la risposta
-                    await DisplayAlert("Risposta", "No, non è possibile.", "OK");
-                    break;
-                case 1:
-                    //stampa a video la risposta
-                    await DisplayAlert("Risposta", "Verrà contattato.", "OK");
-                    break;
-                case 2:
-                    //stampa a video la risposta
-                    await DisplayAlert("Risposta", "Per prenotare un auto, serve una carta di credito", "OK");
-                    break;
-                case 3:
-                    //stampa a video la risposta
-                    await DisplayAlert("Risposta", "Deve chiamarci e noi La veniamo a prendere", "OK");
-                    break;
-                case 4:
-                    //stampa a video la risposta
-                    await DisplayAlert("Risposta", "No, non è possibile", "OK");
-                    break;
-                case 5:
-                    //stampa a video la risposta
-                    await DisplayAlert("Risposta", "Sì, è tutto incluso", "OK");
-                    break;
-            }
-        }
     }
 }
+ 
+
 
         
 
